0c32e15efc452cfa9c0a2fcfe30e7ee2f6b028a8
Jesse Eisses <jesse.eisses@gmail.com>
2023-08-10 14:02:29 +0200

    Add delete campaign function

--- src/effect_sdk/campaign.py
+++ src/effect_sdk/campaign.py
@@ -40,3 +40,26 @@ def create(client, ipfs_hash: str, reward: str, task_time: int = 3600):
     )
     resp = client.send_transaction(actions=[action])
     return resp
+
+def delete(client, campaign_id: int):
+    client.require_auth()
+
+    data = [
+        pyntelope.Data(name='campaign_id',value=pyntelope.types.Uint32(campaign_id)),
+        pyntelope.Data(
+            name='owner',
+            value=types.Variant.from_dict(
+                ['name', pyntelope.types.Name(client.auth.actor)],
+                types_=['address', 'name']
+            )
+        ),
+        pyntelope.Data(name='campaign_id', value=pyntelope.types.Uint8(0))
+    ]
+    action = pyntelope.Action(
+        account=client.config['tasks_contract'],
+        name='rmcampaign',
+        data=data,
+        authorization=[client.auth],
+    )
+    resp = client.send_transaction([action])
+    return resp
